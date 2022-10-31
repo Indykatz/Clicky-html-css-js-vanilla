77bcc468d2ab8f244e7a2716b7cf2902d818054d
Indykatz <cats83@me.com>
2022-10-31 13:01:52 +0000

    V1

---
+++ script.js
@@ -0,0 +1,11 @@
+const loremTopics =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, laborum rerum aspernatur consequuntur temporibus ex vitae officia dolore libero, explicabo a ab inventore vel illo alias ullam odit nostrum nihil eos iste expedita quae asperiores voluptas iure! Rerum nisi itaque ipsam, similique soluta fugiat, dolorum dolorem molestias, voluptatibus magni eum.";
+
+function topic1Function() {
+  var x = document.getElementById("topic1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
